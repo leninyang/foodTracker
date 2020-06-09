028df0dc53bf37c06de693b8eebc1fbe199ddc04
Lenin Yang <leninyang@gmail.com>
2020-06-09 10:14:58 -0700

    created database

---
+++ food_tracker.sql
@@ -0,0 +1,22 @@
+#File that creates the tables | 3 Tables
+
+-- Keeping track of dates
+create table log_date(
+	id integer primary key autoincrement,
+	entry_date date not null
+);
+
+-- Keeping track if list of foods
+create table food(
+	id integer primary key autoincrement,
+	name text not null,
+	carbobydrates integer not null,
+	fat integer not null,
+	calories integer not null
+);
+-- Keeping track of food being added to a date.
+create table foo_date(
+	food_id integer  not null,
+	log_date_id integer not null,
+	primary key(food_id, log_date_id)
+);
